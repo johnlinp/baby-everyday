ecda169130f1ec7ab8f452545be9253b3206e31b
John Lin <johnlinp@gmail.com>
2020-07-17 18:23:03 +0800

    Hide the real page in a secret path

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
+import os
 import flask
 
-app = flask.Flask('baby-everyday')
+
+SECRET_PATH = os.getenv('SECRET_PATH', '')
+
+app = flask.Flask('baby-everyday', static_url_path=f'/static/{SECRET_PATH}')
 
 
 @app.route('/')
+def index():
+    return flask.render_template('index.html')
+
+
+@app.route(f'/babies/{SECRET_PATH}')
 def babies():
-    return flask.render_template('babies.html')
+    return flask.render_template('babies.html', secret_path=SECRET_PATH)
