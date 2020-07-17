87faf1a18f916baf637618b728057a0b4e3083f7
John Lin <johnlinp@gmail.com>
2020-07-18 07:45:11 +0800

    Use photoage to generate mappings

--- app.py
+++ app.py
@@ -1,11 +1,66 @@
 import os
+import datetime
 import flask
+import photoage
 
 
 SECRET_PATH = os.getenv('SECRET_PATH', '')
 STARTUP_DAY = int(os.getenv('STARTUP_DAY', '0'))
+LEFT_BIRTHDAY = os.getenv('LEFT_BIRTHDAY')
+RIGHT_BIRTHDAY = os.getenv('RIGHT_BIRTHDAY')
+PHOTOAGE_OFFSET = os.getenv('PHOTOAGE_OFFSET')
+PHOTOAGE_ENABLE_STAT = os.getenv('PHOTOAGE_ENABLE_STAT', 'false') == 'true'
 
 app = flask.Flask('baby-everyday', static_url_path=f'/static/{SECRET_PATH}')
+
+
+def detect_birthday(dir_path, file_names, photoage_enable_stat):
+    get_date_time_options = {}
+    if photoage_enable_stat:
+        get_date_time_options['methods'] = ('exif', 'stat')
+    first_date_time = None
+    for file_name in file_names:
+        file_path = os.path.join(dir_path, file_name)
+        date_time = photoage.get_date_time(file_path, **get_date_time_options)
+        if date_time is None:
+            continue
+        if first_date_time is None or date_time < first_date_time:
+            first_date_time = date_time
+    if first_date_time is not None:
+        return first_date_time.strftime('%Y/%m/%d')
+
+
+def generate_mapping(side, birthday, photoage_offset, photoage_enable_stat):
+    dir_path = os.path.join('static', 'photos', side)
+    file_names = os.listdir(dir_path)
+
+    calculate_days_options = {}
+    if birthday is None:
+        birthday = detect_birthday(dir_path, file_names, photoage_enable_stat)
+    if birthday:
+        calculate_days_options['birthday'] = datetime.datetime.strptime(birthday, '%Y/%m/%d')
+    if photoage_offset:
+        offset_time = datetime.datetime.strptime(photoage_offset, '%H:%M:%S')
+        calculate_days_options['offset'] = datetime.timedelta(hours=offset_time.hour, minutes=offset_time.minute, seconds=offset_time.second)
+    if photoage_enable_stat:
+        calculate_days_options['methods'] = ('exif', 'stat')
+
+    mapping = {}
+    for file_name in file_names:
+        file_path = os.path.join(dir_path, file_name)
+        days = photoage.calculate_days(file_path, **calculate_days_options)
+        mapping[days] = f'/static/{SECRET_PATH}/photos/{side}/{file_name}'
+
+    return mapping
+
+def get_sorry_url():
+    dir_path = os.path.join('static', 'photos', 'sorry')
+    first_file_name, *_ = os.listdir(dir_path)
+    return f'/static/{SECRET_PATH}/photos/sorry/{first_file_name}'
+
+left_mapping = generate_mapping('left', LEFT_BIRTHDAY, PHOTOAGE_OFFSET, PHOTOAGE_ENABLE_STAT)
+right_mapping = generate_mapping('right', RIGHT_BIRTHDAY, PHOTOAGE_OFFSET, PHOTOAGE_ENABLE_STAT)
+sorry_url = get_sorry_url()
 
 
 @app.route('/')
@@ -20,16 +75,4 @@ def babies():
 
 @app.route(f'/babies/{SECRET_PATH}/data.js')
 def mapping():
-    left_mapping = {
-        0: f'/static/{SECRET_PATH}/photos/left/IMG_20200717_232313.jpg',
-        1: f'/static/{SECRET_PATH}/photos/left/IMG_20200717_232332.jpg',
-        3: f'/static/{SECRET_PATH}/photos/left/IMG_20200717_232341.jpg',
-    }
-    right_mapping = {
-        0: f'/static/{SECRET_PATH}/photos/right/IMG_20200717_232221.jpg',
-        1: f'/static/{SECRET_PATH}/photos/right/IMG_20200717_232236.jpg',
-        2: f'/static/{SECRET_PATH}/photos/right/IMG_20200717_232245.jpg',
-        3: f'/static/{SECRET_PATH}/photos/right/IMG_20200717_232256.jpg',
-    }
-    sorry_url = f'/static/{SECRET_PATH}/photos/sorry/IMG_20200717_232425.jpg'
     return flask.render_template('data.js', left_mapping=left_mapping, right_mapping=right_mapping, sorry_url=sorry_url)
